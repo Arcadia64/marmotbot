8a6d3f4c4081510d0da1ef4f5b954ae842f8cd91
Arcadia64 <arcadia62091@gmail.com>
2017-01-16 02:41:14 -0500

    Bot Template

--- bot.js
+++ bot.js
@@ -1,7 +1,18 @@
-var marmot = "I will be back soon";
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-{
+var ytdl = require('ytdl-core');
 
-}
 
-hello hello
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.username}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+bot.login('MjU1NDgzNDg1NTg4OTQ2OTQ0.C1y-cQ.0OEf-lPm5Ya0gFyICrRqbpO9qpA');
